7032e9d82e9fa18ef1bb8c8be1ed6ddf7d6ac241
e-value <y.miyata@env-value.co.jp>
2025-12-21 21:45:35 +0900

    feat: 401エラー時の自動ログインリダイレクト機能を追加
    - Axiosレスポンスインターセプターを実装
    - 未認証（401エラー）時に自動的にログイン画面へリダイレクト
    - セッション切れやトークン無効時のUX改善

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,20 @@
 import axios from "axios";
+import router from "./router";
+
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.withXSRFToken = true;
+
+// 401エラー（未認証）の場合、ログイン画面にリダイレクト
+window.axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            // 認証が必要なのにログインしていない場合
+            router.push({ name: "login" });
+        }
+        return Promise.reject(error);
+    }
+);
