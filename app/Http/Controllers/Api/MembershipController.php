d96d488e94b22c320871f1050c363288e5dad619
inaba_env <a.inaba@env-value.co.jp>
2025-12-25 18:16:34 +0900

    feat: コントローラーの追加

---
+++ app/Http/Controllers/Api/MembershipController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MembershipController extends Controller
+{
+    public function index()
+    {
+        abort(501, 'Not Implemented');
+    }
+    public function store()
+    {
+        abort(501, 'Not Implemented');
+    }
+    
+    public function show()
+    {
+        abort(501, 'Not Implemented');
+    }
+
+    public function update()
+    {
+        abort(501, 'Not Implemented');
+    }
+
+    public function destroy()
+    {
+        abort(501, 'Not Implemented');
+    }
+}
