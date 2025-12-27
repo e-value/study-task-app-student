0fd60ab728b829b9f7e2100944ecee3bf972bb70
e-value <y.miyata@env-value.co.jp>
2025-12-27 12:07:20 +0900

    fix: UserFactoryにfake()関数のuse文を追加してHerokuデプロイエラーを修正

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use function Illuminate\Support\fake;
 
 use function Illuminate\Support\fake;
 
