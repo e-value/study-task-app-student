38451b7fff5154e1712496e22045c074261d19e8
OKUMURA-env <n.okumura@env-value.co.jp>
2025-12-29 11:29:59 +0900

    APIResource

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'is_archived' => $this->is_archived,
+            'users' => $this->whenLoaded('users'),
+            'tasks' => $this->whenLoaded('tasks'),
+        ];
+    }
+}
