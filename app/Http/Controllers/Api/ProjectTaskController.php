cdaa94884f9a75b481b701f40e09d8dfc82d2fdf
ruishishi <r.shishido@env-value.co.jp>
2025-12-30 10:39:08 +0900

    コントローラーの修正

---
+++ app/Http/Controllers/Api/ProjectTaskController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class ProjectTaskController extends Controller
+{
+    public function index($project)
+    {
+        abort(501, 'Not Implemented');
+    }
+    public function store($project)
+    {
+        abort(501, 'Not Implemented');
+    }
+    public function show($project, $task)
+    {
+        abort(501, 'Not Implemented');
+    }
+    public function update($project, $task)
+    {
+        abort(501, 'Not Implemented');
+    }
+    public function destroy($project, $task)
+    {
+        abort(501, 'Not Implemented');
+    }
+
+    public function start($project, $task)
+    {
+        abort(501, 'Not Implemented');
+    }
+    public function complete($project, $task)
+    {
+        abort(501, 'Not Implemented');
+    }
+
+    public function getUserAllTasks($user)
+    {
+        abort(501, 'Not Implemented');
+    }
+}
