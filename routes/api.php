cd6d7faddcda204472fdc2f84048684a74f6fd73
okada_env <k.okada@env-value.co.jp>
2025-12-30 22:12:49 +0900

    Step 1: エンドポイント設計

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,70 @@ Route::middleware(['auth:sanctum'])->group(function () {
         return $request->user();
     });
 });
+
+
+#プロジェクトの一覧取得
+Route::get('/projects', [ProductController::class, 'index']);
+
+#プロジェクトの作成
+Route::post('/projects', [ProductController::class, 'create']);
+
+#プロジェクトの詳細取得
+Route::get('/projects/{project}', [ProductController::class, 'store']);
+
+#プロジェクトの更新
+Route::put('/projects/{project}', [ProductController::class, 'update']);
+
+#プロジェクトの削除
+Route::delete('/projects/{project}',  [ProductController::class, 'delete']);
+
+#プロジェクトのメンバー追加
+Route::post('/projects/{project}/members', [MembershipsController::class,'add']);
+
+#プロジェクトのタスク一覧取得
+Route::get('/projects/{project}/tasks', [TasksController::class,'index']);
+
+#プロジェクトのメンバーを削除
+Route::delete('/projects/{project}/members', [MembershipsController::class,'delete']);
+
+#プロジェクトにタスク作成
+Route::post('/projects/{project}/tasks', [TasksController::class, 'create']);
+
+#プロジェクトのタスク詳細取得
+Route::get('/projects/{project}/tasks/{task}', [TasksController::class, 'store']);
+
+#プロジェクトのタスクを削除
+Route::delete('/projects/{project}/tasks/{task}', [TasksController::class,'delete']);
+
+#プロジェクトのタスクを更新
+Route::put('/projects/{project}/tasks/{task}', [TasksController::class,'update']);
+
+#プロジェクトのタスクを開始
+Route::post('/projects/{project}/tasks/{task}/start',  [TasksController::class,'start']);
+
+#プロジェクトのタスクを完了
+Route::post('/projects/{project}/tasks/{task}/complete',  [TasksController::class,'complete']);
+
+#自分のタスク一覧取得（自分が所属しているプロジェクトの全タスク）
+Route::get('/me/tasks', [TaskController::class,'index']);
+
+#httpメソッド
+#get:情報取得
+#pot:情報送信・作成
+#put:更新・置換
+#delete:削除
+#patch:部分更新
+
+#一覧＊index
+#詳細*store
+#作成*create
+#削除*delete
+#追加*add
+#開始*start
+#完了*complete
+
+
+#【質問】
+# ① putとpatchの違いは？どのタイミングで使用するのか。
+# ② URLは長くてわかりやすいのか、短くて少しわかりずらいのがよいのか？
+#   ※最初の実装と2回目の実装でURLを迷って変えた。
