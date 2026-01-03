331125813825d3ebbb0c0eddbc7ed0c7d8d7720f
ruishishi <r.shishido@env-value.co.jp>
2026-01-04 08:52:29 +0900

    Lesson1実装

---
+++ app/Http/Controllers/Api/ProjectMemberController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class ProjectMemberController extends Controller
+{
+    public function store($project)
+    {
+        abort(501, 'Not Implemented');
+    }
+
+    public function destroy($project, $member)
+    {
+        abort(501, 'Not Implemented');
+    }
+}
