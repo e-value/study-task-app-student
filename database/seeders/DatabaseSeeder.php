a70d9487b75298d00b338f2a631f3329c9bb19a1
e-value <y.miyata@env-value.co.jp>
2026-01-07 08:25:15 +0900

    feat: Postman導入ガイドと固定トークンシーダーを追加
    - POSTMAN_SETUP_GUIDE.md: Postman導入完全ガイドを作成
    - database/seeders/TestTokenSeeder.php: 固定トークンを生成するシーダーを追加
    - database/seeders/DatabaseSeeder.php: TestTokenSeederを呼び出すように更新
    - README.md: Postman導入ガイドへのリンクを追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             ProjectSeeder::class,
             MembershipSeeder::class,
             TaskSeeder::class,
+            TestTokenSeeder::class,
         ]);
     }
 }
